ce9911a521ce5081507fa7d0c06d36c5771c9715
DrSpyros <74768648+DrSpyros@users.noreply.github.com>
2024-08-09 00:18:57 +0300

    added literally like Dialogue animation stuff

--- Scripts/Game Scripts/Autoload.gd
+++ Scripts/Game Scripts/Autoload.gd
@@ -1,1 +1,17 @@
-extends Node
+extends Script
+
+var Characters = {
+	#true = male, false = female
+	#all characters are depicted as 18+ and not hot children
+	"DrSpyros": true, #Hot principal
+	"wolel": false, #Hot teacher
+	"lieo": true, #The only one with a girlfriend
+	"liea": false, #lieo's girlfriend
+	"MarkMayhem": false, #The club leader
+	"Bread Boys": true, #The club members (simps)
+	"Adam": true, #The one with a giant dick
+	"botorfj": false, #The nerd quiet, also small balls,
+	"endie": true, #Big black dude
+	"Gambinho": false #She didnt specify
+	
+}
