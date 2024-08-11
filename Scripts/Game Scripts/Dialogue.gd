7791e08aace97b18208101c8e35f8c1711386440
DrSpyros <74768648+DrSpyros@users.noreply.github.com>
2024-08-11 14:47:00 +0300

    Fixed Dialogue stuff
    - Fixed Name not showing
    - Fixed the thing where when dialogue played there was some random ass text at the midpoint of the scene

--- Scripts/Game Scripts/Dialogue.gd
+++ Scripts/Game Scripts/Dialogue.gd
@@ -1,21 +1,20 @@
-extends Label
+extends ColorRect
 
-@onready var TextBox = self
-@onready var NameBox = $DialogueName
-@onready var SpriteBox = $Sprite2D
+signal SetText(DialogueText)
 signal SetTexture(CharTexture)
+signal SetName(CharName)
 
 func _ready():
 	await get_tree().create_timer(1).timeout
 	SendDialogue("very cool dialogue", "DrSpyros", "Neutral")
 
 func SendDialogue(DialogueText, Name, State):
-	NameBox = Name
 	var CharTexture = "res://Images/CharacterSprites/" + str(Name) + "/" + str(State) + ".png"
 	emit_signal("SetTexture", CharTexture)
+	emit_signal("SetName", Name)
 	for Index in DialogueText.length():
 		var CutText = DialogueText.erase(Index, DialogueText.length())
-		self.text = CutText
+		emit_signal("SetText", CutText)
 		await get_tree().create_timer(0.05).timeout
 		
-	self.text = DialogueText
+	emit_signal("SetText", DialogueText)
