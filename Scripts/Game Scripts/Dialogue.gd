094b986cbaf5025cacee3b293c830fe85f554112
DrSpyros <spyrosskyllas@hotmail.com>
2024-08-13 13:52:44 +0300

    Improved Dialogue a shitton
    Made calling Dialogue work with Dictionaries and made it more smooth with animations

--- Scripts/Game Scripts/Dialogue.gd
+++ Scripts/Game Scripts/Dialogue.gd
@@ -15,34 +15,51 @@ func _process(delta):
 	if visible == true:
 		if AnimationPlayed == false:
 			AnimationPlayed = true
+			DialogueAnimation.play("StartDialogue")
 	else:
 		AnimationPlayed = false
-
-func SendDialogue(DialogueText, Name, State):
-	visible = true
-	
-	DialogueTexture.texture = load("res://Images/CharacterSprites/" + str(Name) + "/" + str(State) + ".png")
-	DialogueAnimation.play("StartDialogue")
-	DialogueName.text = Name
-	DialogueBox.text = ""
-	await get_tree().create_timer(0.5).timeout
-	for Index in DialogueText.length():
-		var CutText = DialogueText.erase(Index, DialogueText.length())
-		DialogueBox.text = CutText
-		var CurrentLetter = DialogueText.erase(Index + 1, DialogueText.length() + 1)
-		CurrentLetter = CurrentLetter.erase(0, Index)
-		if CurrentLetter == " ":
-			DialogueTexture.texture = load("res://Images/CharacterSprites/" + str(Name) + "/" + str(State) + ".png")
-		else:
-			DialogueTexture.texture = load("res://Images/CharacterSprites/" + str(Name) + "/" + "Talking_" + str(State) + ".png")
-		if CurrentLetter == ".":
-			DialogueTexture.texture = load("res://Images/CharacterSprites/" + str(Name) + "/" + str(State) + ".png")
-			await get_tree().create_timer(0.2).timeout
-		await get_tree().create_timer(0.05).timeout
 		
-	DialogueBox.text = DialogueText
-	DialogueTexture.texture = load("res://Images/CharacterSprites/" + str(Name) + "/" + str(State) + ".png")
-
+func SendDialogue(DialogueDic: Dictionary, Name):
+	visible = true
+	var Dialogues: Array = DialogueDic.keys()
+	var LastState: String = ""
+	for CurrentDialogueIndex: int in DialogueDic.size():
+		
+		var DialogueText: String = Dialogues[CurrentDialogueIndex]
+		var State: String = DialogueDic[DialogueText]
+		
+		if AnimationPlayed == true:
+			if State != LastState:
+				await get_tree().create_timer(0.1).timeout
+				DialogueAnimation.play("DialogueTransition")
+				await get_tree().create_timer(0.04).timeout
+		DialogueTexture.texture = load("res://Images/CharacterSprites/" + str(Name) + "/" + str(State) + ".png")
+		DialogueName.text = Name
+		DialogueBox.text = ""
+		await get_tree().create_timer(0.5).timeout
+		for Index in DialogueText.length():
+			var CutText = DialogueText.erase(Index, DialogueText.length())
+			DialogueBox.text = CutText
+			var CurrentLetter = DialogueText.erase(Index + 1, DialogueText.length() + 1)
+			CurrentLetter = CurrentLetter.erase(0, Index)
+			if CurrentLetter == " ":
+				DialogueTexture.texture = load("res://Images/CharacterSprites/" + str(Name) + "/" + str(State) + ".png")
+			else:
+				DialogueTexture.texture = load("res://Images/CharacterSprites/" + str(Name) + "/" + "Talking_" + str(State) + ".png")
+			if CurrentLetter == ".":
+				DialogueTexture.texture = load("res://Images/CharacterSprites/" + str(Name) + "/" + str(State) + ".png")
+				await get_tree().create_timer(0.2).timeout
+			await get_tree().create_timer(0.05).timeout
+			
+		DialogueBox.text = DialogueText
+		DialogueTexture.texture = load("res://Images/CharacterSprites/" + str(Name) + "/" + str(State) + ".png")
+		await get_tree().create_timer(2.5).timeout
+		LastState = State
+	
+	EndDialogue()
+	
 func EndDialogue():
 	var AnimationPlayed = false
+	DialogueAnimation.play_backwards("StartDialogue")
+	await get_tree().create_timer(0.3).timeout
 	visible = false
--- Scripts/Player Scripts/PlayerMovement.gd
+++ Scripts/Player Scripts/PlayerMovement.gd
@@ -6,8 +6,15 @@ extends CharacterBody2D
 
 func get_input():
 	if Input.is_action_just_pressed("Interact"):
-		AL.DialogueScene.SendDialogue("Hello this is some fun text. whooho", "DrSpyros", "Neutral")
-	var input_direction = Vector2.ZERO
+		var Dialogues = {
+			"Dialogue yay": "Neutral",
+			"This is the second part of my Dialogue": "Neutral",
+			"This is the third dialogue omg!!": "Angry",
+			"Andddd, the alst one. pog": "Neutral"
+		}
+		AL.DialogueScene.SendDialogue(Dialogues, "DrSpyros")
+		
+	var input_direction: Vector2 = Vector2.ZERO
 	var FinalSpeed = Speed
 	if CanMove == true:
 		input_direction = Input.get_vector("Left", "Right", "Up", "Down")
